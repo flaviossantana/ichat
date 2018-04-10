987cc924040e3f89263374e410edbd04affa0376
flavio.santana <9090flavio@gmail.com>
2018-04-09 21:45:35 -0300

    Enviando nossa mensagens

---
+++ app/src/main/java/br/com/alura/ichat/service/IChatService.java
@@ -0,0 +1,17 @@
+package br.com.alura.ichat.service;
+
+import br.com.alura.ichat.modelo.Mensagem;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface IChatService {
+
+    @POST("polling")
+    Call<Void> envair(@Body Mensagem mensagem);
+
+    @GET("polling")
+    Call<Void> ouvirMensagem();
+
+}
